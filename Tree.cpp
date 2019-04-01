b657bd861360b6498e6f90f76d16154e06d29d77
rawnaq94 <33788325+rawnaq94@users.noreply.github.com>
2019-04-01 03:15:58 +0300

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -1,14 +1,81 @@
-// Part One with partial implementation ---- for running only //
+// Part Two with full exercise //
 
+#include <iostream>
 #include "Tree.hpp"
+using namespace std;
 
 namespace ariel{
     
 
-Tree::Tree() { }
+Tree::Tree() // copy constructor
+{
+  rootOfTree = NULL;
+  value = 0;
+}
+    
+    
+    
+Tree::~Tree() //Deletes a vertex if it is Null
+{
+    if (rootOfTree==NULL) 
+       {
+           delete rootOfTree;
+       }
+    else 
+       {
+           destroy(rootOfTree); 
+       }
+}     
+Tree& Tree::insert(int i) 
+{ 
+    if (contains(i)) //First search the number if found to throw error
+       {  
+          throw(" error syntax the tree contains the number")
+       }
 
-Tree Tree::insert(int i) { return *this; }
+    NodeT *newNodeT=new NodeT(i); //Creates a vertex
+    bool flag=false;
+    
+    if (rootOfTree==NULL) 
+       {
+           rootOfTree=newNodeT;
+           newNodeT->right=NULL;
+           newNodeT->left=NULL;
+       }
+     else // current vertex is a root of the tree
+       {
+           NodeT *vertex=rootOfTree;
+           while(vertex !NULL && !flag)
+                {
+                     if(i<vertex->num)
+                       {
+                          if(vertex->left==NULL)
+                            {
+                                vertex->left=newNodeT;
+                                flag=true;
+                            }
+                         vertex=vertex->left;
+                       }
+                     else
+                       {
+                          if(i>vertex->num)
+                            {
+                               if(vertex->right==NULL)
+                                 {
+                                    vertex->right=newNodeT;
+                                    flag=true;
+                                 }
+                              vretex=vertex->right;
+                             }
+                        }
+                  }
+     }
+    value++;
+    return *this;
+}
 
+    
+    
 Tree Tree::remove(int i) { return *this; }
 
 int Tree::size() { return 0; }
@@ -24,5 +91,6 @@ int Tree::left(int i) { return 0; }
 int Tree::right(int i){ return 0; }
     
 void Tree::print() { }
+
     
 };
